9208628de85807ed283fd290d9cb94896e4f6029
Alana Soares :) <alana.fs@hotmail.com>
2021-10-28 14:26:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    time: 5000 // tempo de transição dos slides
+  })
+
+  new SimpleSlide({
+    slide: 'portfolio',
+    time: 5000,
+    nav: true,
+    pauseOnHover: true
+  })
+}
+
+if (window.SimpleAnime) {
+  new SimpleAnime()
+}
